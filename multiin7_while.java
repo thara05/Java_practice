b2384679e4cb100aed43f24402c428a21db48a92
THARANI M <152951908+thara05@users.noreply.github.com>
2026-02-16 14:00:45 +0530

    Add files via upload

---
+++ multiin7_while.java
@@ -0,0 +1,20 @@
+package exp1;
+
+public class multiin7_while {
+
+	public multiin7_while() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(i<=10) {
+			System.out.println("7 * "+i+"="+7*i);
+		i++;
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ multitab_11.java
@@ -0,0 +1,17 @@
+package exp1;
+
+public class multitab_11 {
+
+	public multitab_11() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		for(int i =0;i <=10;i++) {
+			System.out.println("11 * "+i+" = "+11*i);
+		}
+
+	}
+
+}
