81bcccbd6758360ef0b3462fc9ec56b0109d3d8a
THARANI M <152951908+thara05@users.noreply.github.com>
2026-02-24 21:13:12 +0530

    Add files via upload

---
+++ looping.java
@@ -0,0 +1,23 @@
+package exp1;
+import java.util.Scanner;
+
+public class looping {
+
+	public looping() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		int i =1;
+		while(i<=10){
+			System.out.println("9 * "+i+ " = " + 9*i);
+			i++;
+		
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
