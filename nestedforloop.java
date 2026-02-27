be677d281c4922e33e5656a25077a71f92102c1e
THARANI M <152951908+thara05@users.noreply.github.com>
2026-02-27 21:52:55 +0530

    Add files via upload

---
+++ nestedforloop.java
@@ -0,0 +1,23 @@
+	package exp1;
+	
+	public class nestedforloop {
+	
+		public nestedforloop() {
+			// TODO Auto-generated constructor stub
+		}
+	
+	
+		public static void main(String[] args) {
+		int   i,j;
+		for(i=1;i<=4;i++){
+			for(j=1;j<=3;j++){
+				System.out.print(i);
+				{
+					System.out.println();// TODO Auto-generated method stub
+				}
+			}
+		}
+	
+		}
+	
+	}
