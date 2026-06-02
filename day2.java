bf6a2ed146d90be8d83e8633c1f96aa75f427882
THARANI M <152951908+thara05@users.noreply.github.com>
2026-06-02 09:42:43 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,21 @@
+package placementclass;
+
+public class day2 {
+
+	public day2() {
+		
+	}
+
+	public static void main(String[] args) {
+		int[]arr = {1,2,3,4,5,6,7};
+		int sum = 0;
+		for(int i = 0;i<arr.length;i++) {
+			sum = sum + arr[i];
+			
+		}
+		System.out.println("count is " + sum);
+		
+
+	}
+
+}
