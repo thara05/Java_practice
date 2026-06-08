90182f1396cd452a2e473cd46156ffcffe12c290
THARANI M <152951908+thara05@users.noreply.github.com>
2026-06-08 11:27:15 +0530

    Add exception handling in main method

---
+++ Exceptionhandling.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class exceptionhandling {
+
+	public exceptionhandling() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+  {		        try
+		        {
+		            int A[] = {10, 0, 8, 3, 5};
+		            int r;
+		            r = A[0] / A[1];
+		            System.out.println(r);
+
+		            System.out.println(A[10]);
+		        }
+		        catch (ArithmeticException e)
+		        {
+		            System.out.println(e);
+		        }
+		        catch (ArrayIndexOutOfBoundsException e)
+		        {
+		            System.out.println(e);
+		        }
+		        System.out.println("End of Program");
+		    }
+		
+	}
+	
