6c7f374d97675ba31817df5effd286a597d6e856
THARANI M <152951908+thara05@users.noreply.github.com>
2026-06-11 11:39:02 +0530

    Add files via upload

---
+++ votingeligible.java
@@ -0,0 +1,23 @@
+package exp1;
+
+public class votingeligible {
+
+	public votingeligible() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String name = "Thara";
+		System.out.print(name);
+		int age = 19;
+		if(age >= 18)
+		{
+			System.out.print(" is eligible for voting");
+		}
+		else
+		{
+			System.out.print(" is not eligible for voting");
+		}
+	}
+
+}
---
+++ vowels.java
@@ -0,0 +1,30 @@
+package exp1;
+
+public class vowels {
+
+	public vowels() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String letter ="s";
+		switch(letter)
+		{
+		    case "a":
+		    case "e":
+		    case "i":    	
+		    case "o":
+		    case "u":
+		
+			System.out.print("vowels");
+			break;
+		
+		default:
+			System.out.print("consonant");
+			
+		
+
+	}
+
+	}
+}
